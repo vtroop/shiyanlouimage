c1f584451db9ecb5106eedba24bb8b62e0e56574
Evilina <vtroop@126.com>
2016-09-06 00:24:23 +0800

    aspectj与事务支持

---
+++ src/main/java/com/aspectj/LogAspect.java
@@ -0,0 +1,13 @@
+package com.aspectj;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Evilina on 2016/8/30.
+ */
+@Aspect
+@Component
+public class LogAspect {
+
+}
